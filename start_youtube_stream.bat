110bca592716cda9e1650cd6b8b741c1a98dedba
ja1152452 <jayzelyasona23@gmail.com>
2026-08-18 21:35:56 +0800

    ihi

--- start_youtube_stream.bat
+++ start_youtube_stream.bat
@@ -8,16 +8,20 @@ set RTSP_URL=rtsp://FloodMonitoring:FloodCam2026@192.168.1.149:554/stream1
 
 :loop
 echo [%time%] Pushing live camera stream to YouTube...
-ffmpeg -nostdin -loglevel warning ^
+ffmpeg -nostdin -loglevel error ^
   -rtsp_transport tcp ^
+  -timeout 10000000 ^
+  -use_wallclock_as_timestamps 1 ^
+  -thread_queue_size 4096 ^
+  -fflags +genpts+discardcorrupt ^
+  -err_detect ignore_err ^
   -i "%RTSP_URL%" ^
-  -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 ^
-  -map 0:v:0 -map 1:a:0 ^
-  -c:v copy ^
-  -c:a aac -b:a 128k ^
-  -f flv "rtmp://a.rtmp.youtube.com/live2/%STREAM_KEY%"
+  -c:v libx264 -preset ultrafast -tune zerolatency -r 30 -g 30 -keyint_min 30 -sc_threshold 0 ^
+  -s 1280x720 -b:v 2500k -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p ^
+  -c:a aac -b:a 128k -ar 44100 -af "aresample=async=1" ^
+  -f flv -flvflags no_duration_filesize "rtmp://a.rtmp.youtube.com/live2/%STREAM_KEY%"
 
-echo [%time%] Connection dropped. Reconnecting in 2s...
+echo [%time%] RTSP/YouTube connection dropped. Reconnecting in 2s...
 timeout /t 2 /nobreak >nul
 goto loop
 
