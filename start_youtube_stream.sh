66cfd693b0b8462c02453b7f351133c185b517b4
ja1152452 <jayzelyasona23@gmail.com>
2026-08-18 20:07:22 +0800

    Add YouTube stream bash script and update camera IP

---
+++ start_youtube_stream.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "==================================================="
+echo "Starting Live Tapo C310 Stream to YouTube on Raspberry Pi"
+echo "==================================================="
+
+STREAM_KEY="0t7b-p7t1-1ds5-a64k-4tp5"
+RTSP_URL="rtsp://FloodMonitoring:FloodCam2026@192.168.1.149:554/stream1"
+
+while true; do
+  echo "[$(date +'%T')] Pushing live camera stream to YouTube..."
+  ffmpeg -nostdin -loglevel error \
+    -rtsp_transport tcp \
+    -timeout 10000000 \
+    -use_wallclock_as_timestamps 1 \
+    -thread_queue_size 4096 \
+    -fflags +genpts+discardcorrupt \
+    -err_detect ignore_err \
+    -i "$RTSP_URL" \
+    -c:v libx264 -preset ultrafast -tune zerolatency -r 30 -g 30 -keyint_min 30 -sc_threshold 0 \
+    -s 1280x720 -b:v 2500k -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p \
+    -c:a aac -b:a 128k -ar 44100 -af "aresample=async=1" \
+    -f flv -flvflags no_duration_filesize "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
+
+  echo "[$(date +'%T')] Connection dropped. Reconnecting in 2s..."
+  sleep 2
+done
